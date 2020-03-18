bda5972eca40eea740928946650eb134c39de22d
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-03-18 18:50:28 -0400

    callbackuri

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_DOMAIN}
     client_id={process.env.REACT_APP_CLIENT_ID}
-    redirect_uri={window.location.origin}
+    redirect_uri={`${window.location.origin}/callback`}
     audience={process.env.REACT_APP_AUDIENCE}
     onRedirectCallback={onRedirectCallback}
   >
