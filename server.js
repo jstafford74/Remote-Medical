386acd0413cb58259a501f264aeae4fd9c039397
Jeremiah Stafford <52318268+jstafford74@users.noreply.github.com>
2020-03-29 19:17:54 -0400

    change to postbuild script

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(passport.initialize());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 // Add routes, both API and view
 app.use(routes);
